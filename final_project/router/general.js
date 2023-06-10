5e6c61c642e3c8f09b012039e5332f75d83161a4
ineBallardin <ineballardin@gmail.com>
2023-06-10 10:41:07 -0300

    update the book's isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,13 +12,13 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json(JSON.stringify(books, null, 4));
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  return res.status(300).json(books[isbn]);
  });
   
 // Get book details based on author
