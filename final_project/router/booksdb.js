3c2580011313c1455440be021b268585e5b5a338
ineBallardin <ineballardin@gmail.com>
2023-06-10 11:42:04 -0300

    update the books reviews and the code for getting book reviews

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"1": "This is the review for the book Things Fall Apart"} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"1": "This is the review for the book Fairy tales"} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"1": "This is the review for the book The Divine Comedy"} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"1": "This is the review for the book The Epic Of Gilgamesh"} },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"1": "This is the review for the book The Book Of Job"} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {"1": "This is the review for the book One Thousand and One Nights"} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"1": "This is the review for the book Nj\u00e1l's Saga"} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"1": "This is the review for the book Pride and Prejudice"} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"1": "This is the review for the book Le P\u00e8re Goriot"} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"1": "This is the review for the book Molloy, Malone Dies, The Unnamable, the trilogy"} }
 }
 
 module.exports=books;
