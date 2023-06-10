6f5e790c0331ef1d04b645e14df7be322d718f57
ineBallardin <ineballardin@gmail.com>
2023-06-10 16:28:58 -0300

    Improving the scope of general.js using Promises or Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,75 +1,86 @@
 const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
+
+const books = require("./booksdb.js");
+const { isValid, users } = require("./auth_users.js");
+
 const public_users = express.Router();
 
 const doesExist = (username) => {
-  return users.some(user => user.username === username);
-}
+  return users.some((user) => user.username === username);
+};
 
 public_users.post("/register", (req, res) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password) {
-      return res.status(400).json({ message: "Unable to register user." });
-    }
-  
-    if (!isValid(username, password)) {
-      return res.status(400).json({ message: "Provided credentials are not valid." });
-    }
-  
-    if (doesExist(username)) {
-      return res.status(409).json({ message: "User already exists!" });
-    }
-  
-    users.push({ username, password });
-    return res.status(200).json({ message: "User successfully registered. Now you can login" });
-  });
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Unable to register user." });
+  }
+
+  if (!isValid(username, password)) {
+    return res
+      .status(400)
+      .json({ message: "Provided credentials are not valid." });
+  }
+
+  if (doesExist(username)) {
+    return res.status(409).json({ message: "User already exists!" });
+  }
+
+  users.push({ username, password });
+  return res
+    .status(200)
+    .json({ message: "User successfully registered. Now you can login" });
+});
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  let isbn = req.params.isbn
-  return res.status(200).json(books[isbn]);
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({ message: "Book not found." });
+  }
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const bookKeys = Object.keys(books)
-  const author = req.params.author
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
 
-  const filteredBooks = bookKeys.filter((key) => {
-    return books[key].author === author;
-  }).map((key) => {
-    return books[key];
-  });
+  const filteredBooks = Object.values(books).filter(
+    (book) => book.author === author
+  );
 
-  return res.status(300).json(filteredBooks);
+  return res.status(200).json(filteredBooks);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const bookKeys = Object.keys(books)
-  const title = req.params.title
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
 
-  const filteredBooks = bookKeys.filter((key) => {
-    return books[key].title === title;
-  }).map((key) => {
-    return books[key];
-  });
+  const filteredBooks = Object.values(books).filter(
+    (book) => book.title === title
+  );
 
-  return res.status(300).json(filteredBooks);
+  return res.status(200).json(filteredBooks);
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  const isbn = req.params.isbn
-  return res.status(300).json(books[isbn].reviews);
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book && book.reviews) {
+    return res.status(200).json(book.reviews);
+  } else {
+    return res.status(404).json({ message: "Reviews not found for the book." });
+  }
 });
 
 module.exports.general = public_users;
